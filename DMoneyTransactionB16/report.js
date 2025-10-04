cb1ddf759b4a60daeafc9ab84a916bd6a711c4c2
Tamim Ahasan Rijon <tamimahasan.ar@gmail.com>
2025-10-05 00:11:54 +0600

    Refactor report.js to use local collection file instead of Postman API link; add DMoneyTransactionB16 collection JSON file

--- DMoneyTransactionB16/report.js
+++ DMoneyTransactionB16/report.js
@@ -2,7 +2,8 @@ const newman = require('newman');
 require('dotenv').config();
 
 newman.run({
-  collection: 'https://api.postman.com/collections/44392319-b523db42-fc1f-43cb-83b0-74ad89aaf4b4?access_key=' + process.env.PMAT_KEY,
+  // collection: 'https://api.postman.com/collections/44392319-b523db42-fc1f-43cb-83b0-74ad89aaf4b4?access_key=' + process.env.PMAT_KEY,
+  collection:require('./collection/DMoneyTransactionB16.json'),
   reporters: 'htmlextra',
   iterationCount: 1,
   reporter: {
