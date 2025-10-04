d2fb80af180590d0998847905d417edd1451256e
Tamim Ahasan Rijon <tamimahasan.ar@gmail.com>
2025-10-04 23:52:14 +0600

    Add initial project setup with package.json and report script
    - Created package.json with dependencies for dotenv, newman, and newman-reporter-htmlextra.
    - Added report.js to run Newman tests and generate HTML report.

---
+++ DMoneyTransactionB16/report.js
@@ -0,0 +1,16 @@
+const newman = require('newman');
+require('dotenv').config();
+
+newman.run({
+  collection: 'https://api.postman.com/collections/44392319-b523db42-fc1f-43cb-83b0-74ad89aaf4b4?access_key=' + process.env.PMAT_KEY,
+  reporters: 'htmlextra',
+  iterationCount: 1,
+  reporter: {
+    htmlextra: {
+      export: './Reports/report.html',
+    },
+  },
+}, function (err) {
+  if (err) { throw err; }
+  console.log('Collection run complete!');
+});
