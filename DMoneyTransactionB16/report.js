bc51bb24e173f9a26a2db53c37a233807e125c55
Tamim Ahasan Rijon <tamimahasan.ar@gmail.com>
2025-10-05 00:20:25 +0600

    Comment out collection URL and local JSON file in report.js

--- DMoneyTransactionB16/report.js
+++ DMoneyTransactionB16/report.js
@@ -2,8 +2,11 @@ const newman = require('newman');
 require('dotenv').config();
 
 newman.run({
+  
   // collection: 'https://api.postman.com/collections/44392319-b523db42-fc1f-43cb-83b0-74ad89aaf4b4?access_key=' + process.env.PMAT_KEY,
-  collection:require('./collection/DMoneyTransactionB16.json'),
+
+  // collection:require('./collection/DMoneyTransactionB16.json'),
+
   reporters: 'htmlextra',
   iterationCount: 1,
   reporter: {
